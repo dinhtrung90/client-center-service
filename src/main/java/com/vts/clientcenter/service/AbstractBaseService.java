6d5d1dbec379d31aafd1700ea8d5278bc33df983
Trung Vu <trung.vu@adurolife.com>
2021-05-18 08:12:06 +0700

    - add permission handler
    - add permission handler

--- src/main/java/com/vts/clientcenter/service/AbstractBaseService.java
+++ src/main/java/com/vts/clientcenter/service/AbstractBaseService.java
@@ -1,18 +1,22 @@
 package com.vts.clientcenter.service;
 
 import com.vts.clientcenter.config.Constants;
-import com.vts.clientcenter.domain.User;
-import com.vts.clientcenter.repository.RolePermissionRepository;
-import com.vts.clientcenter.repository.UserRepository;
+import com.vts.clientcenter.domain.*;
+import com.vts.clientcenter.domain.enumeration.OperationEnum;
+import com.vts.clientcenter.repository.*;
 import com.vts.clientcenter.security.SecurityUtils;
 import com.vts.clientcenter.web.rest.errors.BadRequestAlertException;
+import com.vts.clientcenter.web.rest.errors.UnAuthorizedRequestAlertException;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 @Service
 @Transactional
@@ -26,6 +30,15 @@ public class AbstractBaseService {
 
     @Autowired
     private RolePermissionRepository rolePermissionRepository;
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @Autowired
+    private PermissionOperationRepository permissionOperationRepository;
+
+    @Autowired
+    private ModuleOperationRepository moduleOperationRepository;
 
     private Supplier<User> userLogin = this::getUserByLogin;
 
@@ -42,18 +55,45 @@ public class AbstractBaseService {
         }
         return userOptional.get();
     }
-    //    private boolean hasPermission(String roleName, PermissionAction action) {
-    //        User user = getUserByLogin();
-    //        user.getAuthorities().stream()
-    //        .forEach( r -> {
-    //            List<Long> collectPermission = r.getPermissions().stream().map(Permission::getId).collect(Collectors.toList());
-    //            List<RolePermission> permissionList = rolePermissionRepository.findAllByPermission_idIn(collectPermission);
-    //
-    //            permissionList.stream()
-    //                .filter(rolePermission -> rolePermission.getRoleName().equalsIgnoreCase(roleName))
-    //                .filter(rolePermission -> rolePermission.)
-    //
-    //        });
-    //    }
 
+    public boolean hasPermission(String roleName, String permission, List<OperationEnum> operations) {
+        User user = getUserByLogin();
+
+        List<Authority> authorities = user
+            .getAuthorities()
+            .stream()
+            .filter(r -> r.getName().equalsIgnoreCase(roleName))
+            .collect(Collectors.toList());
+
+        if (CollectionUtils.isEmpty(authorities)) {
+            throw new UnAuthorizedRequestAlertException("User is not authorized.", "UserRole", Constants.USER_UNAUTHORIZED);
+        }
+
+        Authority authority = authorities.get(0);
+
+        // fetching permission By Roles
+
+        List<RolePermission> rolePermissions = rolePermissionRepository.findByRoleName(authority.getName());
+
+        Optional<RolePermission> rolePermissionOptional = rolePermissions.stream().filter(r -> isValid(r, permission)).findFirst();
+
+        if (!rolePermissionOptional.isPresent()) {
+            throw new UnAuthorizedRequestAlertException("Permission not supported yet.", "UserRole", Constants.PERMISSION_NOT_SUPPORTED);
+        }
+
+        RolePermission rolePermission = rolePermissionOptional.get();
+
+        List<PermissionOperation> permissionOperations = permissionOperationRepository.findByRolePermissionId(rolePermission.getId());
+
+        List<Long> idOperations = permissionOperations.stream().map(PermissionOperation::getOperationId).collect(Collectors.toList());
+
+        List<ModuleOperation> operationInRoles = moduleOperationRepository.findAllById(idOperations);
+
+        return operationInRoles.stream().map(ModuleOperation::getName).collect(Collectors.toSet()).containsAll(operations);
+    }
+
+    private boolean isValid(RolePermission r, String permission) {
+        Permission permissionObj = permissionRepository.getOne(r.getPermissionId());
+        return permissionObj.getName().equalsIgnoreCase(permission);
+    }
 }
