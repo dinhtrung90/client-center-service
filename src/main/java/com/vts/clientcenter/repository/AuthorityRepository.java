1945b83a9ec40ce5331a395e4d6cc8cb9887b659
Trung Vu <trung.vu@adurolife.com>
2021-05-21 08:40:22 +0700

    add roles

--- src/main/java/com/vts/clientcenter/repository/AuthorityRepository.java
+++ src/main/java/com/vts/clientcenter/repository/AuthorityRepository.java
@@ -1,9 +1,11 @@
 package com.vts.clientcenter.repository;
 
 import com.vts.clientcenter.domain.Authority;
+import com.vts.clientcenter.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * Spring Data JPA repository for the {@link Authority} entity.
  */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
+public interface AuthorityRepository extends JpaRepository<Authority, String>, JpaSpecificationExecutor<Authority> {}
