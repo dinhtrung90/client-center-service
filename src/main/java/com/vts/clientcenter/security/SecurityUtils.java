7940c7872aa5c3b85a1b2d37dbe903767af9c9fd
Trung Vu <trung.vu@adurolife.com>
2021-05-24 10:50:08 +0700

    Revert "add roles"
    This reverts commit 1945b83a9ec40ce5331a395e4d6cc8cb9887b659.

--- src/main/java/com/vts/clientcenter/security/SecurityUtils.java
+++ src/main/java/com/vts/clientcenter/security/SecurityUtils.java
@@ -36,7 +36,7 @@ public final class SecurityUtils {
             UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
             return springSecurityUser.getUsername();
         } else if (authentication instanceof JwtAuthenticationToken) {
-            return (String) ((JwtAuthenticationToken) authentication).getToken().getClaims().get("email");
+            return (String) ((JwtAuthenticationToken) authentication).getToken().getClaims().get("preferred_username");
         } else if (authentication.getPrincipal() instanceof DefaultOidcUser) {
             Map<String, Object> attributes = ((DefaultOidcUser) authentication.getPrincipal()).getAttributes();
             if (attributes.containsKey("preferred_username")) {
